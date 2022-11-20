3a507b9fee3b3e0c973904f910b59d04e8bfd150
Robson Ramos <robinhokcond1@hotmail.com>
2022-11-20 14:43:46 -0300

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author UTF8
+ *
+ */
+module primeiro_programa_java {
+}
---
+++ src/primeiro_programa_java/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package primeiro_programa_java;
+
+import primeiro_programa_java.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+	    System.out.println(livros);
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
